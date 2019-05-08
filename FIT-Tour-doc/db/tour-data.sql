5c002089789f9df41853d620fe161244d614a2f9
SeungBeen Oh <osb1992@naver.com>
2019-05-08 18:10:44 +0900

    .

---
+++ FIT-Tour-doc/db/tour-data.sql
@@ -0,0 +1,15 @@
+insert into tour(tour_id, title, sub_heading, content, created_date, total_hour, hash_tag, personnel, transportation, price)
+values(1, '가우디투어', '신나는 여행', '내용입니다.', '2019-05-01', 9, 'FFAZ', 3, '버스', '120000');
+
+insert into tour(tour_id, title, sub_heading, content, created_date, total_hour, hash_tag, personnel, transportation, price)
+values(2, '뉴욕투어', '즐거운 여행', '내용입니다.', '2019-01-01', 9, 'ZZAZ', 3, '버스', '120000');
+
+insert into tour(tour_id, title, sub_heading, content, created_date, total_hour, hash_tag, personnel, transportation, price)
+values(3, '오사카투어', '자전거 여행', '내용입니다.', '2019-02-01', 9, 'WWAZ', 3, '지하철', '120000');
+
+insert into tour(tour_id, title, sub_heading, content, created_date, total_hour, hash_tag, personnel, transportation, price)
+values(4, '부산투어', '야경 여행', '내용입니다.', '2019-10-01', 9, 'REAZ', 3, '버스', '120000');
+
+insert into tour(tour_id, title, sub_heading, content, created_date, total_hour, hash_tag, personnel, transportation, price)
+values(5, '바르셀로나투어', '도보 여행', '내용입니다.', '2019-08-01', 9, 'FFAQ', 3, '도보', '120000');
+

2ba53a2108b8d3205f3e10303fd808a4fdd05523
hskim2019 <hwasun.kim2019@gmail.com>
2019-05-09 11:18:35 +0900

    tour_comment

--- FIT-Tour-doc/db/tour_comment-data.sql
+++ FIT-Tour-doc/db/tour_comment-data.sql
@@ -1,14 +1,14 @@
-insert into tour(member_id, tour_id, ranking, level, content, created_date)
+insert into tour_comment(member_id, tour_id, ranking, level, content, created_date)
 values(101, 1, 1, 1, '내용입니다.' ,'2019-02-15 10:22:24');
 
-insert into tour(member_id, tour_id, ranking, level, content, created_date)
+insert into tour_comment(member_id, tour_id, ranking, level, content, created_date)
 values(102, 2, 1, 1, '내용입니다2.' ,'2019-02-15 10:22:24');
 
-insert into tour(member_id, tour_id, ranking, level, content, created_date)
+insert into tour_comment(member_id, tour_id, ranking, level, content, created_date)
 values(103, 3, 1, 1, '내용입니다3.' ,'2019-02-15 10:22:24');
 
-insert into tour(member_id, tour_id, ranking, level, content, created_date)
+insert into tour_comment(member_id, tour_id, ranking, level, content, created_date)
 values(104, 4, 1, 1, '내용입니다4.' ,'2019-02-15 10:22:24');
 
-insert into tour(member_id, tour_id, ranking, level, content, created_date)
+insert into tour_comment(member_id, tour_id, ranking, level, content, created_date)
 values(105, 5, 1, 1, '내용입니다5.' ,'2019-02-15 10:22:24');

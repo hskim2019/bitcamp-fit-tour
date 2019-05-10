068af3948e6202e428ab8471e8afda7c94138335
SeungBeen Oh <osb1992@naver.com>
2019-05-10 14:24:05 +0900

    tour_theme.sql

---
+++ FIT-Tour-doc/db/tour_theme.sql
@@ -0,0 +1,16 @@
+insert into tour_theme(tour_id, theme_id)
+values(1, 1);
+insert into tour_theme(tour_id, theme_id)
+values(1, 6);
+insert into tour_theme(tour_id, theme_id)
+values(2, 3);
+insert into tour_theme(tour_id, theme_id)
+values(3, 1);
+insert into tour_theme(tour_id, theme_id)
+values(3, 2);
+insert into tour_theme(tour_id, theme_id)
+values(4, 6);
+insert into tour_theme(tour_id, theme_id)
+values(5, 3);
+insert into tour_theme(tour_id, theme_id)
+values(5, 4);

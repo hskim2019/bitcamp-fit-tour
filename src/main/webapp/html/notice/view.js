4f98ae0c9416e9b975e92e6d624e781cc05c379a
meeyoungchoi <goodlife1359@gmail.com>
2019-05-10 15:32:14 +0900

    notice.viewjs

--- src/main/webapp/html/notice/view.js
+++ src/main/webapp/html/notice/view.js
@@ -1,133 +1,70 @@
-var param = location.href.split('?')[1],
-
-addBtn = $('#add-btn'),
-updateBtn = $('#update-btn'),
-deleteBtn = $('#delete-btn'),
-no = $('#no'),
-title = $('#title'),
-content = $('#content'),
-viewCount = $('#viewCount'),
-createdDate = $('#createdDate'),
-templateSrc = $('#tr-template').html();
-
+var param = location.href.split('?')[1];
 
 
 if (param) {
- 
   $('h1').html('게시물 보기');
-  loadData(param.split('=')[1])
+  loadData(param.split('=')[1]);
   var el =  $('.bit-new-item');
   for (e of el) {
     e.style.display = 'none';
   }
 } else {
-
   $('h1').html('새글');
-
   var el = $('.bit-view-item');
   for (e of el) {
-   e.style.display = 'none';
+    e.style.display = 'none';
   }
 }
-document.querySelector('#add-btn').onclick = () => {
-
-  var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4 || xhr.status != 200)
-      return;
-    
-    var data = JSON.parse(xhr.responseText);
-    
-    if (data.status == 'success') {
-      location.href = "index.html"
-        
-    } else {
-      alert('등록 실패입니다!\n' + data.message)
-    }
-  };
-  xhr.open('POST', '../../app/json/notice/add', true)
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-  var title = document.querySelector('#title').value;
-  var content = document.querySelector('#content').value;
-  
-  xhr.send("title=" + encodeURIComponent(title)
-      +"&content=" + encodeURIComponent(content)
-  );
-  
-};
-
-document.querySelector('#delete-btn').onclick = () => {
-  var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4 || xhr.status != 200)
-      return;
-    
-    var data = JSON.parse(xhr.responseText);
-    
-    if (data.status == 'success') {
-      location.href = "index.html"
-        
-    } else {
-      alert('삭제 실패입니다!\n' + data.message)
-    }
-  };
-  var no = document.querySelector('#no').value;
-  xhr.open('GET', '../../app/json/notice/delete?no=' + no, true)
-  xhr.send();
-};
-
-document.querySelector('#update-btn').onclick = () => {
-  var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4 || xhr.status != 200)
-      return;
-    
-    var data = JSON.parse(xhr.responseText);
-    
-    if (data.status == 'success') {
-      location.href = "index.html"
-        
-    } else {
-      alert('변경 실패입니다!\n' + data.message)
-    }
-  };
-  xhr.open('POST', '../../app/json/notice/update', true)
-  xhr.setRequestHeader("Content-Type", "application/x-www-form-urlencoded");
-  
-  var no = document.querySelector('#no').value;
-  var title = document.querySelector('#title').value;
-  var content = document.querySelector('#content').value;
-  
-  var qs = 'title=' + encodeURIComponent(title)+
-  
-    '&content=' + encodeURIComponent(content) +
-  
-    '&no=' + no;
-  xhr.send(qs);
-};
-
 function loadData(no) {
-  var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4 || xhr.status != 200)
-      return;
-    
-    var data = JSON.parse(xhr.responseText);
-    console.log(data);
-    document.querySelector('#no').value = data.no;
-    document.querySelector('#content').value = data.content;
-     document.querySelector('#title').value = data.title;
-    document.querySelector('#createdDate').value = data.createdDate;
-    document.querySelector('#viewCount').value = data.viewCount;
-  };
-  xhr.open('GET', '../../app/json/notice/detail?no=' + no, true)
-  xhr.send()
-}
+  $.getJSON('../../app/json/notice/detail?no=' + param.split('=')[1], function(data) {
+    $('#no').val(data.no);
+    $('#content').val(data.content);
+    $('#title').val(data.title);
+    $('#createdDate').val(data.createdDate);
+    $('#viewCount').val(data.viewCount);
+  });
+};
+$('#add-btn').click(() => {
+  $.post('../../app/json/notice/add', {
 
+    title: $('#title').val(),
+    content: $('#content').val()
+  },
 
+  function(data) {
+    headers: ("Content-Type", "application/x-www-form-urlencoded");
+  if(data.status == 'success') {
+    location.href = "index.html";  
+  } 
+  }).fail(function(data){
+    alert('등록 실패 입니다.\n' + data.message);
+  })
+});
 
+$('#delete-btn').click(() => {
+  $.getJSON('../../app/json/notice/delete?no=' + param.split('=')[1], function(data) {
+    if(data.status == 'success') {
+      location.href = "index.html";  
+    } else {
+      alert('삭제 실패 입니다.\n', + data.message);
+    }
+  })
+});
 
-
+$('#update-btn').click (() => {
+ 
+  $.post('../../app/json/notice/update?no=' + param.split('=')[1],{
+    title: $('#title').val(),
+    content: $('#content').val()
+  },
+  function(data) {
+    headers: ("Content-Type", "application/x-www-form-urlencoded");
+  if(data.status == 'success') {
+    location.href = "index.html";  
+  } 
+  }).fail(function(data){
+    alert('등록 실패 입니다.\n' + data.message);
+  })
+});
 
 

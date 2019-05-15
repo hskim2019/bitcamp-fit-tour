a51d6403c31dc8265094cb54bf4b48cea3d23cf0
LeeSolMoe <warwick8300@gmail.com>
2019-05-15 20:33:09 +0900

    singup

--- src/main/webapp/html/auth/signup.js
+++ src/main/webapp/html/auth/signup.js
@@ -34,36 +34,16 @@ $('#add-btn').click(() => {
     }
   });
 });
-$("#id").keyup(function(){
-  //입력한 문자열을 읽어온다.
-  var email=$(this).val();
-  //ajax 요청을 해서 서버에 전송한다.
-  $.getJSON('../../app/json/signup/emailoverlap?email='+email, 
-          function(data) {
-          var obj=data.email;
-          if(id != obj){//사용 가능한 아이디 라면 
-              $("#overlapErr").hide();
-              // 성공한 상태로 바꾸는 함수 호출
-              successState("#id");
-              
-          }else{//사용 가능한 아이디가 아니라면 
-              $("#overlapErr").show();
-              errorState("#id");
-          }
-          
-  });
-});
-
 
 
 $("#pwd").keyup(function(){
   var pwd=$(this).val();
   // 비밀번호 검증할 정규 표현식
   var reg=/^.{8,}$/;
-  if(reg.test(pwd)){//정규표현식을 통과 한다면
+  if(reg.test(pwd)){// 정규표현식을 통과 한다면
               $("#pwdRegErr").hide();
               successState("#pwd");
-  }else{//정규표현식을 통과하지 못하면
+  }else{// 정규표현식을 통과하지 못하면
               $("#pwdRegErr").show();
               errorState("#pwd");
   }
@@ -72,35 +52,44 @@ $("#rePwd").keyup(function(){
   var rePwd=$(this).val();
   var pwd=$("#pwd").val();
   // 비밀번호 같은지 확인
-  if(rePwd==pwd){//비밀번호 같다면
+  if(rePwd==pwd){// 비밀번호 같다면
       $("#rePwdErr").hide();
       successState("#rePwd");
-  }else{//비밀번호 다르다면
+  }else{// 비밀번호 다르다면
       $("#rePwdErr").show();
       errorState("#rePwd");
   }
 });
 $("#email").keyup(function(){
+  $("#overlapErr").hide();
   var email=$(this).val();
   // 이메일 검증할 정규 표현식
   var reg=/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-  if(reg.test(email)){//정규표현식을 통과 한다면
+  
+  if(reg.test(email)){// 정규표현식을 통과 한다면
               $("#emailErr").hide();
               successState("#email");
-  }else{//정규표현식을 통과하지 못하면
+  }else{// 정규표현식을 통과하지 못하면
               $("#emailErr").show();
               errorState("#email");
   }
+  $.getJSON('../../app/json/signup/emailoverlap?email='+email, 
+          function(data) {
+
+  if(email == data.email){// 사용 가능한 아이디가 아니라면
+    $("#overlapErr").show();
+    errorState("#email");
+  }
+  
+  });
+  
+  
 });
 // 성공 상태로 바꾸는 함수
 function successState(sel){
   $(sel)
-  .parent()
-  .removeClass("has-error")
-  .addClass("has-success")
-  .find(".glyphicon")
-  .removeClass("glyphicon-remove")
-  .addClass("glyphicon-ok")
+  .removeClass("is-invalid")
+  .addClass("is-valid")
   .show();
 
   $("#myForm button[type=submit]")
@@ -109,13 +98,8 @@ function successState(sel){
 // 에러 상태로 바꾸는 함수
 function errorState(sel){
   $(sel)
-  .parent()
-  .removeClass("has-success")
-  .addClass("has-error")
-  .find(".glyphicon")
-  .removeClass("glyphicon-ok")
-  .addClass("glyphicon-remove")
-  .show();
+.removeClass("is-valid")
+  .addClass("is-invalid")
 
   $("#myForm button[type=submit]")
               .attr("disabled","disabled");

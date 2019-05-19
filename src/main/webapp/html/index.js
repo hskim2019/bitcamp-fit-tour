4bfcdaa81cb56354d985847655b096cdc96836d6
OhSeungBeen <osb1992@naver.com>
2019-05-19 11:18:00 +0900

    index.html

--- src/main/webapp/html/index.js
+++ src/main/webapp/html/index.js
@@ -1,26 +1,3 @@
-// header.html을 로딩하고 초기화시킨다.
-// 헤더 가져오기
-(function () {
-  var header = document.querySelector('.bit-main-header');
-  var xhr = new XMLHttpRequest()
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState != 4 || xhr.status != 200)
-      return;
-    header.innerHTML = xhr.responseText
-    
-    // body 태그 쪽에 헤더 로딩 완료 이벤트를 보낸다.
-    var e = new Event('loaded.header');
-    document.body.dispatchEvent(e);
-  };
-  xhr.open('GET', '/bitcamp-fit-tour/html/header.html', true)
-  xhr.send()
-})();
-
-// header.html의 내용을 웹 페이지에 삽입했다고 연락이 오면,
-// 즉시 로그아웃 버튼에 click listener를 등록한다.
-// header.html이 삽입되지도 않았는데 로그아웃 버튼을 찾아서는 안된다.
-//
-document.body.addEventListener('loaded.header', () => {
   
   // 웹 페이지에 header.html을 삽입했으면 로그인 정보를 가져와 설정한다.
   loadLoginUser();
@@ -39,7 +16,6 @@ document.body.addEventListener('loaded.header', () => {
     xhr.open('GET', '/bitcamp-fit-tour/app/json/auth/logout', true)
     xhr.send()
   });
-});
 
 // header.html이 웹 페이지에 삽입된 후 로그인 정보를 받아온다. 
 function loadLoginUser() {

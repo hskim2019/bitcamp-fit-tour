d15fd53dc02766677af5678e0749a4ed1bdae905
LeeSolMoe <warwick8300@gmail.com>
2019-05-20 13:00:20 +0900

    .

--- src/main/java/com/eomcs/lms/conf/MailConfig.java
+++ src/main/java/com/eomcs/lms/conf/MailConfig.java
@@ -3,7 +3,6 @@ package com.eomcs.lms.conf;
 import java.util.Properties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.context.annotation.PropertySource;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.JavaMailSenderImpl;
 

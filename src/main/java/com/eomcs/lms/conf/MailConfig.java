3088f456905fd9ff0678748208cc0aa5210fcc95
LeeSolMoe <warwick8300@gmail.com>
2019-05-20 10:49:55 +0900

    country

---
+++ src/main/java/com/eomcs/lms/conf/MailConfig.java
@@ -0,0 +1,45 @@
+package com.eomcs.lms.conf;
+
+import java.util.Properties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+
+// DispatcherServlet의 IoC 컨테이너가 준비해야 할 객체에 대한 정보.
+// => app-servlet.xml의 설정을 이 클래스가 대체한다.
+// 
+@Configuration 
+@PropertySource("classpath:/com/eomcs/lms/conf/mail.properties")
+public class MailConfig {
+  
+  @Bean
+  public JavaMailSender getJavaMailSender() {
+      JavaMailSenderImpl mailSender = new JavaMailSenderImpl();
+      mailSender.setHost("smtp.gmail.com");
+      mailSender.setPort(587);
+       
+      mailSender.setUsername("my.gmail@gmail.com");
+      mailSender.setPassword("password");
+       
+      Properties props = mailSender.getJavaMailProperties();
+      props.put("mail.transport.protocol", "smtp");
+      props.put("mail.smtp.auth", "true");
+      props.put("mail.smtp.starttls.enable", "true");
+      props.put("mail.debug", "true");
+       
+      return mailSender;
+  }
+  
+
+}
+
+
+
+
+
+
+
+
+

99004c83292682c592d326d210a84168afb360ea
hskim2019 <hwasun.kim2019@gmail.com>
2019-05-21 20:35:46 +0900

    tourlist

--- src/main/java/com/eomcs/lms/web/json/TourController.java
+++ src/main/java/com/eomcs/lms/web/json/TourController.java
@@ -15,131 +15,140 @@ import com.eomcs.lms.service.TourService;
 @RequestMapping("/json/tour")
 public class TourController {
 
-  @Autowired TourService tourService;
-  @Autowired TourCommentService tourCommentService;
-  
-  //  tourAdd
-  //  @PostMapping("add")
-  //  public Object add(Tour tour) {
-  //    HashMap<String,Object> content = new HashMap<>();
-  //    try {
-  //      tourService.add(tour);
-  //      content.put("status", "success");
-  //    } catch (Exception e) {
-  //      content.put("status", "fail");
-  //      content.put("message", e.getMessage());
-  //    }
-  //    return content;
-  //  }
-  //  
-  //  @GetMapping("delete")
-  //  public Object delete(int no) {
-  //  
-  //    HashMap<String,Object> content = new HashMap<>();
-  //    try {
-  //      if (tourService.delete(no) == 0) 
-  //        throw new RuntimeException("해당 번호의 게시물이 없습니다.");
-  //      content.put("status", "success");
-  //      
-  //    } catch (Exception e) {
-  //      content.put("status", "fail");
-  //      content.put("message", e.getMessage());
-  //    }
-  //    return content;
-  //  }
-  //  
-  
-  //tourDetail
-  @GetMapping("detail")
-  public Object detail(int no,
-      @RequestParam(defaultValue="1") int pageNo,
-      @RequestParam(defaultValue="3") int pageSize,
-      @RequestParam(defaultValue="0") int addDeleteCount) {
+	@Autowired TourService tourService;
+	@Autowired TourCommentService tourCommentService;
 
-    if (pageSize < 3 || pageSize > 8) 
-      pageSize = 3;
-    
-    int rowCount = tourCommentService.countCommentbyTourNo(no, 0);
-    int totalPage;
-    
-    if(rowCount == 0) {
-      totalPage = 0;
-      pageSize = 0;
-    } else {
-      
-      totalPage = rowCount / pageSize;
-      if (rowCount % pageSize > 0)
-        totalPage++;
-    }
-    
-//    if (pageNo < 1) 
-//      pageNo = 1;
-//    else if (pageNo > totalPage)
-//      pageNo = totalPage;
-    
+	//  tourAdd
+	//  @PostMapping("add")
+	//  public Object add(Tour tour) {
+	//    HashMap<String,Object> content = new HashMap<>();
+	//    try {
+	//      tourService.add(tour);
+	//      content.put("status", "success");
+	//    } catch (Exception e) {
+	//      content.put("status", "fail");
+	//      content.put("message", e.getMessage());
+	//    }
+	//    return content;
+	//  }
+	//  
+	//  @GetMapping("delete")
+	//  public Object delete(int no) {
+	//  
+	//    HashMap<String,Object> content = new HashMap<>();
+	//    try {
+	//      if (tourService.delete(no) == 0) 
+	//        throw new RuntimeException("해당 번호의 게시물이 없습니다.");
+	//      content.put("status", "success");
+	//      
+	//    } catch (Exception e) {
+	//      content.put("status", "fail");
+	//      content.put("message", e.getMessage());
+	//    }
+	//    return content;
+	//  }
+	//  
 
-    HashMap<String, Object> map = new HashMap<>();
-    Tour tour = tourService.get(no);
-    List<TourComment> tourComments = tourCommentService.get(no, pageNo, pageSize, addDeleteCount, 0);
-    int commentAmount = tourCommentService.countCommentbyTourNo(no, 0);
+	//tourDetail
+	@GetMapping("detail")
+	public Object detail(int no,
+			@RequestParam(defaultValue="1") int pageNo,
+			@RequestParam(defaultValue="3") int pageSize,
+			@RequestParam(defaultValue="0") int addDeleteCount) {
 
-    map.put("pageNo", pageNo);
-    map.put("pageSize", pageSize);
-    map.put("totalPage", totalPage);
-    map.put("tour", tour);
-    map.put("commentAmount", commentAmount);
-    map.put("tourComment", tourComments);
-    
-    return map;
-  }
+		if (pageSize < 3 || pageSize > 8) 
+			pageSize = 3;
 
-  //tourList
-  @GetMapping("list")
-  public Object list(
-		  String countryName,
-		  String cityName,
-      @RequestParam(defaultValue="1") int pageNo,
-      @RequestParam(defaultValue="3") int pageSize) {
+		int rowCount = tourCommentService.countCommentbyTourNo(no, 0);
+		int totalPage;
 
-    if (pageSize < 3 || pageSize > 8) 
-      pageSize = 3;
+		if(rowCount == 0) {
+			totalPage = 0;
+			pageSize = 0;
+		} else {
 
-    int rowCount = tourService.size();
-    int totalPage = rowCount / pageSize;
-    if (rowCount % pageSize > 0)
-      totalPage++;
+			totalPage = rowCount / pageSize;
+			if (rowCount % pageSize > 0)
+				totalPage++;
+		}
 
-    if (pageNo < 1) 
-      pageNo = 1;
-    else if (pageNo > totalPage)
-      pageNo = totalPage;
+		//    if (pageNo < 1) 
+		//      pageNo = 1;
+		//    else if (pageNo > totalPage)
+		//      pageNo = totalPage;
 
-    List<Tour> tours = tourService.list(countryName, cityName, pageNo, pageSize);
 
-    HashMap<String,Object> content = new HashMap<>();
-    content.put("list", tours);
-    content.put("pageNo", pageNo);
-    content.put("pageSize", pageSize);
-    content.put("totalPage", totalPage);
+		HashMap<String, Object> map = new HashMap<>();
+		Tour tour = tourService.get(no);
+		List<TourComment> tourComments = tourCommentService.get(no, pageNo, pageSize, addDeleteCount, 0);
+		int commentAmount = tourCommentService.countCommentbyTourNo(no, 0);
 
-    return content;
-  }
-  
-  //  tourUpdate
-  //  @PostMapping("update")
-  //  public Object update(Tour tour) {
-  //    HashMap<String,Object> content = new HashMap<>();
-  //    try {
-  //      if (tourService.update(tour) == 0) 
-  //        throw new RuntimeException("해당 번호의 게시물이 없습니다.");
-  //      content.put("status", "success");
-  //      
-  //    } catch (Exception e) {
-  //      content.put("status", "fail");
-  //      content.put("message", e.getMessage());
-  //    }
-  //    return content;
-  //  }
+		map.put("pageNo", pageNo);
+		map.put("pageSize", pageSize);
+		map.put("totalPage", totalPage);
+		map.put("tour", tour);
+		map.put("commentAmount", commentAmount);
+		map.put("tourComment", tourComments);
+
+		return map;
+	}
+
+	//tourList
+	@GetMapping("list")
+	public Object list(
+			String countryName,
+			String cityName,
+			@RequestParam(defaultValue="1") int pageNo,
+			@RequestParam(defaultValue="3") int pageSize) {
+
+		String searchCountryName = null;
+		String searchCityName = null;
+
+		if (countryName.length() > 0) 
+			searchCountryName = countryName;
+		
+		if (cityName.length() > 0) 
+			searchCityName = cityName;
+
+		if (pageSize < 3 || pageSize > 8) 
+			pageSize = 3;
+
+		int rowCount = tourService.size();
+		int totalPage = rowCount / pageSize;
+		if (rowCount % pageSize > 0)
+			totalPage++;
+
+		if (pageNo < 1) 
+			pageNo = 1;
+		else if (pageNo > totalPage)
+			pageNo = totalPage;
+
+		List<Tour> tours = tourService.list(searchCountryName, searchCityName, pageNo, pageSize);
+
+		HashMap<String,Object> content = new HashMap<>();
+		content.put("list", tours);
+		content.put("pageNo", pageNo);
+		content.put("pageSize", pageSize);
+		content.put("totalPage", totalPage);
+
+		return content;
+	}
+
+	//  tourUpdate
+	//  @PostMapping("update")
+	//  public Object update(Tour tour) {
+	//    HashMap<String,Object> content = new HashMap<>();
+	//    try {
+	//      if (tourService.update(tour) == 0) 
+	//        throw new RuntimeException("해당 번호의 게시물이 없습니다.");
+	//      content.put("status", "success");
+	//      
+	//    } catch (Exception e) {
+	//      content.put("status", "fail");
+	//      content.put("message", e.getMessage());
+	//    }
+	//    return content;
+	//  }
 }
 
 

42d9116270ff15e977a6acddd36a5e6dacac143f
OhSeungBeen <osb1992@naver.com>
2019-05-28 03:05:44 +0900

    touradd

--- src/main/webapp/html/tour/add.js
+++ src/main/webapp/html/tour/add.js
@@ -1,7 +1,7 @@
 $(document).ready(function() {
   $('input[type="text"]').characterCounter();
   $('select').formSelect();
-  $('.tabs').tabs({swipeable: true,});
+  $('.tabs').tabs({duration: 800});
   $('.modal').modal();
 });
 
@@ -10,10 +10,20 @@ $(document).ready(function() {
   var quill = new Quill('#quillEditor', {
   modules: {
     toolbar: [
-      [{ header: [1, 2, false] }],
-      ['bold', 'italic', 'underline'],
       ['image', 'code-block'],
-      [{color: ['red', 'blue']} ],
+      ['bold', 'italic', 'underline', 'strike'],        // toggled buttons
+      ['blockquote', 'code-block'],
+      [{ 'header': 1 }, { 'header': 2 }],               // custom button values
+      [{ 'list': 'ordered'}, { 'list': 'bullet' }],
+      [{ 'script': 'sub'}, { 'script': 'super' }],      // superscript/subscript
+      [{ 'indent': '-1'}, { 'indent': '+1' }],          // outdent/indent
+      [{ 'direction': 'rtl' }],                         // text direction
+      [{ 'size': ['small', false, 'large', 'huge'] }],  // custom dropdown
+      [{ 'header': [1, 2, 3, 4, 5, 6, false] }],
+      [{ 'color': [] }, { 'background': [] }],          // dropdown with defaults from theme
+      [{ 'font': [] }],
+      [{ 'align': [] }],
+      ['clean']                                         // remove formatting button
     ]
   },
   placeholder: 'Compose an epic...',

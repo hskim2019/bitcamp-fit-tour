a70d1919179e2de11e342646eed20cbba8934260
SeungBeen Oh <osb1992@naver.com>
2019-05-29 12:27:07 +0900

    투어 컨트롤러 병합

--- src/main/java/com/eomcs/lms/web/json/TourController.java
+++ src/main/java/com/eomcs/lms/web/json/TourController.java
@@ -1,4 +1,6 @@
 package com.eomcs.lms.web.json;
+import java.awt.image.BufferedImage;
+import java.io.File;
 import java.io.IOException;
 import java.net.URLDecoder;
 import java.util.ArrayList;
@@ -6,9 +8,11 @@ import java.util.Collection;
 import java.util.HashMap;
 import java.util.List;
 import java.util.UUID;
+import javax.imageio.ImageIO;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.Part;
+import org.imgscalr.Scalr;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -121,19 +125,19 @@ public class TourController {
         else if (pageNo > totalPage)
             pageNo = totalPage;
 
-		List<Tour> tours = tourService.list(
-				searchContinentName, searchCountryName, searchCityName, 
-				minPrice, maxPrice
-				, pageNo
-//				, pageSize
-				);
+        List<Tour> tours = tourService.list(
+                searchContinentName, searchCountryName, searchCityName, 
+                minPrice, maxPrice
+                , pageNo
+//              , pageSize
+                );
         
-		HashMap<String,Object> content = new HashMap<>();
-		content.put("list", tours);
-		content.put("pageNo", pageNo);
-//		content.put("pageSize", pageSize);
-		content.put("totalPage", totalPage);
-		content.put("currMaxPrice", currMaxPrice);
+        HashMap<String,Object> content = new HashMap<>();
+        content.put("list", tours);
+        content.put("pageNo", pageNo);
+//      content.put("pageSize", pageSize);
+        content.put("totalPage", totalPage);
+        content.put("currMaxPrice", currMaxPrice);
 
         return content;
     }
@@ -200,7 +204,12 @@ public Object add(HttpServletRequest request /*,@RequestBody String json*/) thro
          String filepath = request.getServletContext().getRealPath(("/upload/tourphoto/" + filename + ".jpg"));
          part.write(filepath+".jpg");
          
-         //makeThumbnail(filepath+".jpg", filename+".jpg", );
+         try {
+          makeThumbnail(filepath, filename);
+        } catch (Exception e) {
+          System.out.println("썸네일 이미지만드는중 에러 발생");
+          e.printStackTrace();
+        }
          
          TourGuidancePhoto tourGuidancePhoto = new TourGuidancePhoto();
          tourGuidancePhoto.setName(filename + "thumbnail.jpg");
@@ -289,22 +298,22 @@ public Object add(HttpServletRequest request /*,@RequestBody String json*/) thro
   //  
   
   
-//  private void makeThumbnail(String filePath, String fileName, String fileExt) throws Exception {
-//    // 저장된 원본파일로부터 BufferedImage 객체를 생성합니다. 
-//    BufferedImage srcImg = ImageIO.read(new File(filePath)); 
-//    // 썸네일의 너비와 높이 입니다. 
-//    int dw = 250, dh = 150; // 원본 이미지의 너비와 높이 입니다. 
-//    int ow = srcImg.getWidth(); 
-//    int oh = srcImg.getHeight(); // 원본 너비를 기준으로 하여 썸네일의 비율로 높이를 계산합니다.
-//    int nw = ow; 
-//    int nh = (ow * dh) / dw; // 계산된 높이가 원본보다 높다면 crop이 안되므로 // 원본 높이를 기준으로 썸네일의 비율로 너비를 계산합니다. 
-//    if(nh > oh) { nw = (oh * dw) / dh; nh = oh; } // 계산된 크기로 원본이미지를 가운데에서 crop 합니다.
-//    BufferedImage cropImg = Scalr.crop(srcImg, (ow-nw)/2, (oh-nh)/2, nw, nh); // crop된 이미지로 썸네일을 생성합니다.
-//    BufferedImage destImg = Scalr.resize(cropImg, dw, dh); // 썸네일을 저장합니다. 이미지 이름 앞에 "THUMB_" 를 붙여 표시했습니다.
-//    String thumbName = PATH + "THUMB_" + fileName; 
-//    File thumbFile = new File(thumbName); 
-//    ImageIO.write(destImg, fileExt.toUpperCase(), thumbFile); 
-//    }
+  private void makeThumbnail(String filePath, String fileName) throws Exception {
+    // 저장된 원본파일로부터 BufferedImage 객체를 생성합니다. 
+    BufferedImage srcImg = ImageIO.read(new File(filePath)); 
+    // 썸네일의 너비와 높이 입니다. 
+    int dw = 250, dh = 150; // 원본 이미지의 너비와 높이 입니다. 
+    int ow = srcImg.getWidth(); 
+    int oh = srcImg.getHeight(); // 원본 너비를 기준으로 하여 썸네일의 비율로 높이를 계산합니다.
+    int nw = ow; 
+    int nh = (ow * dh) / dw; // 계산된 높이가 원본보다 높다면 crop이 안되므로 // 원본 높이를 기준으로 썸네일의 비율로 너비를 계산합니다. 
+    if(nh > oh) { nw = (oh * dw) / dh; nh = oh; } // 계산된 크기로 원본이미지를 가운데에서 crop 합니다.
+    BufferedImage cropImg = Scalr.crop(srcImg, (ow-nw)/2, (oh-nh)/2, nw, nh); // crop된 이미지로 썸네일을 생성합니다.
+    BufferedImage destImg = Scalr.resize(cropImg, dw, dh); // 썸네일을 저장합니다. 이미지 이름 앞에 "THUMB_" 를 붙여 표시했습니다.
+    String thumbName = filePath + "THUMB_" + fileName; 
+    File thumbFile = new File(thumbName); 
+    ImageIO.write(destImg, ".jpg", thumbFile); 
+    }
   
 
 }

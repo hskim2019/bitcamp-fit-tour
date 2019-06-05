d3ba9b54cf8898c7bb4ad4e52016e9051c523a52
SeungBeen Oh <osb1992@naver.com>
2019-06-05 19:34:54 +0900

    유저 프로필 수정중

--- src/main/webapp/html/mypage/mypage.js
+++ src/main/webapp/html/mypage/mypage.js
@@ -1,3 +1,7 @@
+$(document).ready(function(){
+    $('.modal').modal();
+  });
+
 //load user
 if(sessionStorage.getItem('loginUser')){
   var user = JSON.parse(sessionStorage.getItem('loginUser'))
@@ -6,93 +10,78 @@ if(sessionStorage.getItem('loginUser')){
 console.log(user);
 
 //set profile
-$("#profile-picture").attr('src','/bitcamp-fit-tour/upload/member/'+user.photo);
-$("#profile-picture-full").attr('src','/bitcamp-fit-tour/upload/member/'+user.photo);
-$("#info-name").html(user.name);
-$("#info-email").html(user.email);
-$("#info-phone").html(user.tel);
+$('#profile-picture').attr('src','/bitcamp-fit-tour/upload/member/'+user.photo);
+$('#profile-picture-full').attr('src','/bitcamp-fit-tour/upload/member/'+user.photo);
+$('#info-name').html(user.name);
+$('#info-email').html(user.email);
+$('#info-phone').html(user.tel);
 var birth = user.birth.split('-');
 console.log(birth);
-$("#info-birth").html(birth[0] + '년' + birth[1] + '월' + birth[2] + '일');
+$('#info-birth').html(birth[0] + '년' + birth[1] + '월' + birth[2] + '일');
 
-$("#profile-picture-overlay").click(function(){
-  $("#grey-overlay").show();
+$('#profile-picture-overlay').click(function(){
+  $('#grey-overlay').show();
 });
 
-$("#grey-overlay").click(function(){
-    $("#grey-overlay").hide();
+$('#grey-overlay').click(function(){
+    $('#grey-overlay').hide();
 });
 
-$("#edit-info").click(function(){
-  
-  var isEdit=isEditMode();
-  
-  if(isEdit){
-    saveChanges();
-  }
-  else{
-    editInfo();
-  }
-  
-  changeMode(isEdit);
-
+$('#edit-info').click(function(){
 });
 
-function editInfo(){  
-  $(".info-data").each(function(index, data){
-    var $data=$(data);
-    var text = $.trim($data.text());
-    var pattern=getPattern($data);
+
+$('#password-modfiy-btn').click(function(){
+});
+
+
+$("#form-password").validate({
+  rules: {
+    originPassword: {
+        required: true,
+        minlength: 8
+    },
     
-    $data.replaceWith("<input pattern='"+pattern+"' id='"+data.id+"' class='info-input' value='" + text + "'>   </input>");
-  });
-}
+    newPassword: {
+      required: true,
+      minlength: 8,
+    },
+    
+    newPasswordConfirm: {
+      required: true,
+      minlength: 8,
+      equalTo: "#newPassword"
+    },
+    
+  },
 
-function saveChanges(){
-  $(".info-input").each(function(index, data){
-    var $data=$(data);
-    var text = $.trim($data.val());
-    $data.replaceWith("<h id='"+data.id+"' class='info-data'>"+ text +"</h>");
-  });
-}
+messages: {
+    originPassword: {
+      required: '기존 비밀번호를 입력해주세요.',
+      minlength: '8글자 이상 입력하세요.'
+  },
+  newPassword: {
+    required: '새 비밀번호를 입력해주세요.',
+    minlength: '8글자 이상 입력하세요.',
+},
 
-function isEditMode(){
+  newPasswordConfirm: {
+    required: '새 비밀번호를 입력해주세요.',
+    minlength: '8글자 이상 입력하세요.',
+    equalTo: '비밀번호와 일치하지 않습니다. 다시 입력해 주세요'
+}
+},
   
-  var $icon=$("#edit-info > span");
   
-  if($icon.hasClass("fa-edit"))
-  {
-    return false;    
+errorElement : 'div',
+/*errorPlacement: function(error, element) {
+  var placement = $(element).data('error');
+  if (placement) {
+    $(placement).append(error)
+  } else {
+    error.insertAfter(element);
   }
-  return true;
-}
-
-function changeMode(isEdit){
-  var $icon=$("#edit-info > span");
-  
-  $icon.removeClass();
-  
-  if(isEdit){
-    $icon.addClass("fa fa-edit");
-  }else{
-    $icon.addClass("fa fa-check")
-  }
-}
-
-function getPattern($element){
-  var inputType=$element[0].id.split("-")[1];
-  
-  switch(inputType){
-    case "email":
-      return '/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/';
-      break;
-    case "phone":
-      return "/^(?:(?:\+?1\s*(?:[.-]\s*)?)?(?:\(\s*([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9])\s*\)|([2-9]1[02-9]|[2-9][02-8]1|[2-9][02-8][02-9]))\s*(?:[.-]\s*)?)?([2-9]1[02-9]|[2-9][02-9]1|[2-9][02-9]{2})\s*(?:[.-]\s*)?([0-9]{4})(?:\s*(?:#|x\.?|ext\.?|extension)\s*(\d+))?$/";
-      break;
-    case "address":
-      return "*";
-      break;
-  }
-}
+}*/
+});
 
 

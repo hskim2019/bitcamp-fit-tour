36c5312618ee9f0af5589e0a7ced1417e4ae0947
OhSeungBeen <osb1992@naver.com>
2019-06-06 23:44:28 +0900

    유저 프로필 수정중

--- src/main/webapp/html/mypage/mypage.js
+++ src/main/webapp/html/mypage/mypage.js
@@ -1,42 +1,62 @@
-$(document).ready(function(){
-  $('#modal1').modal({
-    onCloseEnd : resetModal
-  });
-});
-
 //load user
 if(sessionStorage.getItem('loginUser')){
   var user = JSON.parse(sessionStorage.getItem('loginUser'))
+  var birthArray = user.birth.split('-');
+  var birth = new Date(birthArray[0],birthArray[1],birthArray[2]);
+  console.log(user);
 }
 
-console.log(user);
+$('#password-modal').modal({
+  onCloseEnd : resetModal
+});
+  
+$('.datepicker').datepicker({
+  format : 'yyyy년 mm월 dd일',
+  defaultDate : birth
+});
+  
 
 //set profile
 $('#profile-picture').attr('src','/bitcamp-fit-tour/upload/member/'+user.photo);
 $('#profile-picture-full').attr('src','/bitcamp-fit-tour/upload/member/'+user.photo);
 $('#info-name').html(user.name);
 $('#info-email').html(user.email);
-$('#info-phone').html(user.tel);
-var birth = user.birth.split('-');
-console.log(birth);
-$('#info-birth').html(birth[0] + '년 ' + birth[1] + '월 ' + birth[2] + '일');
+$('#info-phone').val(user.tel);
+$('#info-birth').val(birthArray[0] + '년 ' + birthArray[1] +'월 ' + birthArray[2] + '일');
+if(user.emailCheck)
+  $('#info-email-agree').attr('checked',true);
+if(user.smsCheck)
+  $('#info-sms-agree').attr('checked',true);
 
+
+// profile edit
+$('#edit-info').click(function(e){
+  editIconTag = $(this).find('i')
+  if(editIconTag.html() == 'edit') {
+    $('#info-birth').attr('disabled',false);
+    $('#info-phone').attr('disabled',false);
+    $('#info-email-agree').attr('disabled',false);
+    $('#info-sms-agree').attr('disabled',false);
+    editIconTag.html('check');
+  } else {
+    $('#info-birth').attr('disabled',true);
+    $('#info-phone').attr('disabled',true);
+    $('#info-email-agree').attr('disabled',true);
+    $('#info-sms-agree').attr('disabled',true);
+    editIconTag.html('check');
+    editIconTag.html('edit');
+  }
+  
+});
+
+
+//profile overlay
 $('#profile-picture-overlay').click(function(){
   $('#grey-overlay').show();
 });
 
 $('#grey-overlay').click(function(){
   $('#grey-overlay').hide();
-});
-
-$('#edit-info').click(function(){
-});
-
-
-
-$('#password-update-cancel-btn').click(function(e){
-  e.preventDefault();
-  $('#modal1').modal('close');
 });
 
 //password update
@@ -74,7 +94,7 @@ $('#password-update-btn').click(function(e){
           }, 
           function(obj) {
             if(obj.status == 'success'){
-              $('#modal1').modal('close');
+              $('#password-modal').modal('close');
               M.toast({ html: '비밀번호가 변경되었습니다.' });
             } else {
               M.toast({ html: obj.message })
@@ -83,6 +103,11 @@ $('#password-update-btn').click(function(e){
           });
 });
 
+//password update cancel
+$('#password-update-cancel-btn').click(function(e){
+  e.preventDefault();
+  $('#password-modal').modal('close');
+});
 
 //password modal reset
 function resetModal(){
@@ -136,6 +161,7 @@ $("#form-password").validate({
   },
 
   errorElement : 'div',
+  
 });
 
 

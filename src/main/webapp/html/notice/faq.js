688439c2b804ffccaf8a9f146b19526d068f31a4
hskim2019 <hwasun.kim2019@gmail.com>
2019-06-10 11:09:32 +0900

    notice

--- src/main/webapp/html/notice/faq.js
+++ src/main/webapp/html/notice/faq.js
@@ -1,75 +1,75 @@
-var pageNo = 1,
-    pageSize = 3,
-    tbodyFaq = $('.tbody-faq'),
-    prevPageLi = $('#prevPage-faq'),
-    nextPageLi = $('#nextPage-faq'),
-    currSpan = $('#currPage-faq > span'),
+var FAQpageNo = 1,
+    FAQpageSize = 3,
+    tbodyFaq = $('.tbodyFAQ'),
+    FAQprevPageLi = $('#prevPageFaq'),
+    FAQnextPageLi = $('#nextPageFaq'),
+    FAQcurrSpan = $('#FAQcurrSpan'),
     FAQtemplateSrc = $('#tr-template-faq').html(); // script 태그에서 템플릿 데이터를 꺼낸다.
 
 //Handlebars를 통해 템플릿 데이터를 가지고 최종 결과를 생성할 함수를 준비한다.
 var FAQtrGenerator = Handlebars.compile(FAQtemplateSrc);
 
 // JSON 형식의 데이터 목록 가져오기
-function loadList(pn) {
+function FAQList(pn) {
   
-  $.getJSON('../../app/json/faq/list?pageNo=' + pn + '&pageSize=' + pageSize, 
-    function(obj) {
+  $.getJSON('../../app/json/faq/list?pageNo=' + pn + '&pageSize=' + FAQpageSize, 
+    function(data) {
       // 서버에 받은 데이터 중에서 페이지 번호를 글로벌 변수에 저장한다.
-      pageNo = obj.pageNo;
+      FAQpageNo = data.faqpageNo;
       
       // TR 태그를 생성하여 테이블 데이터를 갱신한다.
       tbodyFaq.html(''); // 이전에 출력한 내용을 제거한다.
       
       // 템플릿 엔진을 실행하여 tr 태그 목록을 생성한다. 그리고 바로 tbody에 붙인다.
-      $(FAQtrGenerator(obj)).appendTo(tbodyFaq);
+      $(FAQtrGenerator(data)).appendTo(tbodyFaq);
       
       // 현재 페이지의 번호를 갱신한다.
-      currSpan.html(String(pageNo));
+      FAQcurrSpan.html(String(FAQpageNo));
       
       // 1페이지일 경우 버튼을 비활성화 한다.
-      if (pageNo == 1) {
-        prevPageLi.addClass('disabled');
+      if (FAQpageNo == 1) {
+        FAQprevPageLi.addClass('disabled');
       } else {
-        prevPageLi.removeClass('disabled');
+        FAQprevPageLi.removeClass('disabled');
       } 
         
       // 마지막 페이지일 경우 버튼을 비활성화 한다.
-      if (pageNo == obj.totalPage) {
-        nextPageLi.addClass('disabled');
+      if (FAQpageNo == data.faqtotalPage) {
+        FAQnextPageLi.addClass('disabled');
       } else {
-        nextPageLi.removeClass('disabled');
+        FAQnextPageLi.removeClass('disabled');
       }
       
       // 데이터 로딩이 완료되면 body 태그에 이벤트를 전송한다.
-      $(document.body).trigger('loaded-list');
+      $(document.body).trigger('FAQ-list');
       
     }); // Bitcamp.getJSON()
   
-} // loadList()
+} // FAQList()
 
-$('#prevPage-faq > a').click((e) => {
+$('.FAQprevPageLink').click((e) => {
   e.preventDefault();
-  loadList(pageNo - 1);
+  FAQList(FAQpageNo - 1);
 });
 
-$('#nextPage-faq > a').click((e) => {
+$('.FAQnextpageLink').click((e) => {
   e.preventDefault();
-  loadList(pageNo + 1);
+  FAQList(FAQpageNo + 1);
 });
 
 
 //페이지를 출력한 후 1페이지 목록을 로딩한다.
-loadList(1);
+FAQList(1);
 
 // 테이블 목록 가져오기를 완료했으면 제목 a 태그에 클릭 리스너를 등록한다. 
-$(document.body).bind('loaded-list', () => {
-  // 제목을 클릭했을 때 view.html로 전환시키기
-  $('.bit-view-link').click((e) => {
-    e.preventDefault();
-    window.location.href = 'view.html?no=' + 
-      $(e.target).attr('data-no');
-  });
-});
+//$(document.body).bind('loaded-list', () => {
+//  // 제목을 클릭했을 때 view.html로 전환시키기
+//  $('.bit-view-link').click((e) => {
+//    e.preventDefault();
+//    window.location.href = 'view.html?no=' + 
+//      $(e.target).attr('data-no');
+//  });
+//});
 
 //$('.collapsible-notice').click((e) => {
 //	$('.collapsible-notice').addClass('checked');
--- src/main/webapp/html/notice/index.js
+++ src/main/webapp/html/notice/index.js
@@ -1,18 +1,16 @@
 var pageNo = 1,
     pageSize = 3,
-    tbodyNotic = $('.tbody-notice'),
+    tbody = $('.tbodyNotice'),
     prevPageLi = $('#prevPage'),
     nextPageLi = $('#nextPage'),
-    currSpan = $('#currPage-notice > span'),
-    templateSrc = $('#tr-template-notice').html(); // script 태그에서 템플릿 데이터를 꺼낸다.
+    currSpan = $('#currPage > span'),
+    templateSrc = $('#tr-template').html(); // script 태그에서 템플릿 데이터를 꺼낸다.
 
 //Handlebars를 통해 템플릿 데이터를 가지고 최종 결과를 생성할 함수를 준비한다.
 var trGenerator = Handlebars.compile(templateSrc);
-//$(document).ready(function(){
-//    $('.parallax').parallax();
-//  });
+
 // JSON 형식의 데이터 목록 가져오기
-function loadList(pn) {
+function noticeList(pn) {
   
   $.getJSON('../../app/json/notice/list?pageNo=' + pn + '&pageSize=' + pageSize, 
     function(obj) {
@@ -20,10 +18,10 @@ function loadList(pn) {
       pageNo = obj.pageNo;
       
       // TR 태그를 생성하여 테이블 데이터를 갱신한다.
-      tbodyNotic.html(''); // 이전에 출력한 내용을 제거한다.
+      tbody.html(''); // 이전에 출력한 내용을 제거한다.
       
       // 템플릿 엔진을 실행하여 tr 태그 목록을 생성한다. 그리고 바로 tbody에 붙인다.
-      $(trGenerator(obj)).appendTo(tbodyNotic);
+      $(trGenerator(obj)).appendTo(tbody);
       
       // 현재 페이지의 번호를 갱신한다.
       currSpan.html(String(pageNo));
@@ -43,33 +41,28 @@ function loadList(pn) {
       }
       
       // 데이터 로딩이 완료되면 body 태그에 이벤트를 전송한다.
-      $(document.body).trigger('loaded-list');
+      $(document.body).trigger('notice-list');
       
     }); // Bitcamp.getJSON()
   
-} // loadList()
-
-$(document).ready(function(){
-    $('.parallax').parallax();
-  });
-       
+} // noticeList()
 
 $('#prevPage > a').click((e) => {
   e.preventDefault();
-  loadList(pageNo - 1);
+  noticeList(pageNo - 1);
 });
 
 $('#nextPage > a').click((e) => {
   e.preventDefault();
-  loadList(pageNo + 1);
+  noticeList(pageNo + 1);
 });
 
 
 //페이지를 출력한 후 1페이지 목록을 로딩한다.
-loadList(1);
+noticeList(1);
 
 // 테이블 목록 가져오기를 완료했으면 제목 a 태그에 클릭 리스너를 등록한다. 
-$(document.body).bind('loaded-list', () => {
+$(document.body).bind('notice-list', () => {
   // 제목을 클릭했을 때 view.html로 전환시키기
   $('.bit-view-link').click((e) => {
     e.preventDefault();
@@ -83,7 +76,7 @@ $('.collapsible-notice').click((e) => {
 	$('.collapsible-faq').removeClass('checked');
 	$('.faq').addClass('bit-invisible');
 	$('.notice').removeClass('bit-invisible');
-	loadList(1);
+	noticeList(1);
 });
 
 $('.collapsible-faq').click((e) => {
@@ -91,7 +84,7 @@ $('.collapsible-faq').click((e) => {
 	$('.collapsible-notice').removeClass('checked');
 	$('.notice').addClass('bit-invisible');
 	$('.faq').removeClass('bit-invisible');
-	loadList(1);
+	noticeList(1);
 });
 
 
